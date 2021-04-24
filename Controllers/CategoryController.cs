f9d715e1f6a0a2c8ec21e552bf470282ffc81149
vilsonpereira <vilsonpereira@nasajon.com.br>
2021-04-24 18:30:06 -0300

    webAPI202104241830 - WebAPI ## Criação do Controller de categoria

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using testeef.Data;
+using testeef.Models;
+
+namespace testeef.Controllers
+{
+    [ApiController]
+    [Route("v1/Categories")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet] // verbo http (por padrão é GET)
+        [Route("")] //rota ja está especificada no annotation Route; O que estiver aqui é concatenado com o annotation
+        public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context) //FromServices pega o datacontext da memória nao precisa do construtot. **
+        {
+            var categories = await context.Categories.ToListAsync();
+            return categories;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Category>> Post(
+            [FromServices] DataContext context, //recebo do serviço um datacontext pra ser injetado
+            [FromBody] Category model) //do corpo da requisição eu recebo a categoria 
+        {
+            if (ModelState.IsValid) //valida a categoria (aplica todas as validações do model)
+            {
+                context.Categories.Add(model); 
+                await context.SaveChangesAsync(); //salva no banco (context)
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+
+    /*
+     ** old construction:
+        private DataContext _context;
+        
+        public CategoryController(DataContext context)
+        {
+            _context = context;
+        }
+     Essa forma veio para facilitar pois utilizando um annotation no argumento da função vc já tem toda a injeção de dependencia sendo utilizada diretamente
+     */
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # simpleWebAPI
 Criando uma API utilizando ASP.NET Core
+
+
+Para utilizar será necessário utilizar o Postman
+https://localhost:5001/v1/categories
